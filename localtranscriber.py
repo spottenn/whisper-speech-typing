a86ff00266b8f571c7aa87bb93ade55e32c19b2c
spottenn <spottenn@gmail.com>
2024-05-21 12:13:09 -0600

    added asynchronous model loading/warming up.

--- localtranscriber.py
+++ localtranscriber.py
@@ -1,3 +1,5 @@
+import threading
+
 from faster_whisper import WhisperModel
 from transcriber import Transcriber
 from whisperqueue import WhisperQueue
@@ -11,39 +13,27 @@ class LocalTranscriber(Transcriber):
         self.language = language
         self.device = device
         self.message_queue = message_queue
-        try:
-            self.model = WhisperModel(self.model_size, compute_type=self.compute_type, device=self.device)
-        except ValueError as e:
-            print(e)
-            print("Computation type not supported. Defaulting to float32")
-            self.compute_type = "float32"
-            self.model = WhisperModel(self.model_size, compute_type=self.compute_type, device=self.device)
-
-
-        silent_audio = np.zeros(160, dtype=np.float32)
-
-        segments, _ = self.model.transcribe(silent_audio, vad_filter=False, without_timestamps=True, language=self.language)
-        list(segments)
-
-
+        self.model = None
+        self.start_lock = threading.Lock()
+        threading.Thread(target=self.start).start()
 
     def transcribe_audio(self, audio):
+        if self.model is None:
+            self.start()
         segments, _ = self.model.transcribe(audio, vad_filter=True, without_timestamps=True, language=self.language)
         segments_list = list(segments)
-
         if len(segments_list) != 0:
-            full_transcription = " ".join(segment.text for segment in segments_list)
-            return full_transcription
+            full_transcription = "".join(segment.text for segment in segments_list)
+            # Check if the last character is not a whitespace
+            if full_transcription and not full_transcription[-1].isspace():
+                full_transcription += ' '
+
+            # Remove leading whitespace
+            return full_transcription.lstrip()
         print("Error transcribing. Blank audio?")
         return ""
 
-    def post_process(self, input_string):
-        # Check if the last character is not a whitespace
-        if input_string and not input_string[-1].isspace():
-            input_string += ' '
 
-        # Remove leading whitespace
-        return input_string.lstrip()
 
     # TODO: start and stop methods to release resources
 
@@ -52,3 +42,22 @@ class LocalTranscriber(Transcriber):
     def stop(self):
         self.model = None
         pass
+
+    def start(self):
+        with self.start_lock:
+            print("Loading model...")
+            if self.model is not None:
+                return
+            try:
+                self.model = WhisperModel(self.model_size, compute_type=self.compute_type, device=self.device)
+            except ValueError as e:
+                print(e)
+                print("Computation type not supported. Defaulting to float32")
+                self.compute_type = "float32"
+                self.model = WhisperModel(self.model_size, compute_type=self.compute_type, device=self.device)
+            # Warm up the model
+            print("Starting model...")
+            silent_audio = np.zeros(160, dtype=np.float32)
+            segments, _ = self.model.transcribe(silent_audio, vad_filter=False, without_timestamps=True, language=self.language)
+            list(segments)
+            print("Model ready.")
